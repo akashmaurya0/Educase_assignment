e6beda6dca35bcf84b5cf16e69e7de931c038841
adityasriv2317 <adityaxia9237@gmail.com>
2025-08-28 20:14:54 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen w-screen bg-gray-800">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
